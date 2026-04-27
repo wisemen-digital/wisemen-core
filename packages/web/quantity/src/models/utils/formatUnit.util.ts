e0d192bdf6fcb86a560e4802a964b7a65bee955d
Nick Banken <nick.banken@gmail.com>
2026-04-27 12:19:54 +0200

    fix: remove try catch

--- packages/web/quantity/src/models/utils/formatUnit.util.ts
+++ packages/web/quantity/src/models/utils/formatUnit.util.ts
@@ -4,17 +4,12 @@ export function formatUnitValue(
   intlUnit?: string,
 ): string {
   if (intlUnit !== undefined) {
-    try {
-      return new Intl.NumberFormat(undefined, {
-        maximumFractionDigits: 1,
-        style: 'unit',
-        unit: intlUnit,
-        unitDisplay: 'short',
-      }).format(value)
-    }
-    catch {
-      // Some BE units do not have a valid Intl unit mapping in all runtimes.
-    }
+    return new Intl.NumberFormat(undefined, {
+      maximumFractionDigits: 1,
+      style: 'unit',
+      unit: intlUnit,
+      unitDisplay: 'short',
+    }).format(value)
   }
 
   return `${new Intl.NumberFormat(undefined, {
