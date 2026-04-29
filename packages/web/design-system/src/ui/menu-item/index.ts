a0a198541adb38a629a7b4087005855c79e7a6f1
JeroenVanC <jeroen.vc1@icloud.com>
2026-04-29 10:05:11 +0200

    fix: lint issues

--- packages/web/design-system/src/ui/menu-item/index.ts
+++ packages/web/design-system/src/ui/menu-item/index.ts
@@ -1,8 +1,8 @@
 export type { MenuItemProps as UIMenuItemProps } from './menuItem.props'
-export { default as UIMenuItem } from './MenuItem.vue'
 export type {
   MenuItemAvatarConfig as UIMenuItemAvatarConfig,
   MenuItemConfig as UIMenuItemConfig,
   MenuItemDotConfig as UIMenuItemDotConfig,
   MenuItemRightConfig as UIMenuItemRightConfig,
 } from './menuItem.type'
+export { default as UIMenuItem } from './MenuItem.vue'
