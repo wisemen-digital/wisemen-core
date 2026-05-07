875ba1dd2d71cd7e7f1844a7f20fa5da7d542a35
Wouter Laermans <wouter.laermans@appwise.be>
2026-05-07 14:56:58 +0200

    feat(detail-list): add components

---
+++ packages/web/design-system/src/ui/detail-list/DetailList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { UIColumnLayout } from '@wisemen/vue-core-design-system'
+</script>
+
+<template>
+  <UIColumnLayout
+    gap="2xl"
+    class="w-full"
+  >
+    <slot />
+  </UIColumnLayout>
+</template>
