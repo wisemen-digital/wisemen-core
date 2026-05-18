5518105803451f2e03066d63fa70e394965c4473
Kobe Kwanten <kobe.kwanten@wisemen.digital>
2026-05-18 13:05:07 +0200

    feat(datewise): add EndsBeforeOrAt and StartsAfterOrAt typeorm operators

---
+++ packages/api/datewise/lib/common/typeorm/starts-after-or-at.ts
@@ -0,0 +1,48 @@
+import { randomUUID } from 'crypto'
+import { FindOperator, Raw } from 'typeorm'
+import { DateTimeRange } from '../../date-time-range/date-time-range.js'
+import { DateRange } from '../../date-range/date-range.js'
+import { PlainDate } from '#src/plain-date/index.js'
+import { isPlainDate } from '#src/plain-date/is-plain-date.js'
+import { isTimestamp } from '#src/timestamp/is-timestamp.js'
+import { Timestamp } from '#src/timestamp/timestamp.js'
+
+/** 
+ * Checks if a range starts at or after the given range with the `&>` operator. 
+ */
+export function StartsAfterOrAt (date: PlainDate): FindOperator<DateRange>
+export function StartsAfterOrAt (ts: Timestamp | Date): FindOperator<DateTimeRange>
+export function StartsAfterOrAt (
+    value: PlainDate | Timestamp | Date
+): FindOperator<DateRange> | FindOperator<DateTimeRange> {
+    let range: DateTimeRange | DateRange
+    if (isPlainDate(value)) {
+        range = new DateRange(value, value)
+    } else if (isTimestamp(value) || value instanceof Date) {
+        range = new DateTimeRange(value, value, '[]')
+    } else {
+        throw new Error('invalid argument, expected plain date, timestamp or date')
+    }
+
+
+    const paramName = randomUUID().replaceAll('-', '')
+
+    let cast: string
+    if (range instanceof DateRange) {
+        cast = 'daterange'
+    } else {
+        cast = 'tstzrange3'
+    }
+
+    return Raw(
+        (alias: string) => `${alias} &> :${paramName}::${cast}`,
+        { [paramName]: range.toString() }
+    ) as FindOperator<DateRange> | FindOperator<DateTimeRange>
+}
+
+/**
+ * Checks if a range starts on or after the given date with the `&>` operator.
+ */
+export function StartsAfterOrOn (date: PlainDate): FindOperator<DateRange> {
+    return StartsAfterOrAt(date)
+}
