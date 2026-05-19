81f31c29a34e453e5f3aa8c5787502b0719394d8
Wouter Laermans <wouter.laermans@appwise.be>
2026-05-19 10:29:27 +0200

    fix(actions): import

--- packages/web/design-system/src/ui/layout/MainContentSidebarToggle.vue
+++ packages/web/design-system/src/ui/layout/MainContentSidebarToggle.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {
-  _createUntypedAction,
+  createAction,
   GroupPriority,
   useActionGroup,
 } from '@wisemen/vue-core-actions'
@@ -23,7 +23,7 @@ const {
 const i18n = useI18n()
 const actionGroup = useActionGroup()
 
-const toggleMainSidebarAction = _createUntypedAction({
+const toggleMainSidebarAction = createAction({
   id: 'toggle-main-sidebar',
   name: () => i18n.t('action.global.toggle_main_sidebar.name'),
   execute: () => {
