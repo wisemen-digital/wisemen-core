8b8237d426844f594b582599b7433d780525ad4c
Wouter Laermans <wouter.laermans@appwise.be>
2026-05-20 17:42:00 +0200

    feat(permissions): add new package

---
+++ packages/web/permissions/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @wisemen/vue-core-permissions
+
+## 0.0.1
+
+### Features
+
+- Initial release: `PermissionGuard` component and `usePermissionGuard` composable
---
+++ packages/web/permissions/src/index.ts
@@ -0,0 +1,8 @@
+export type { PermissionMatchType } from './permissionGuard.composable'
+export {
+  setPermissions, usePermissionGuard,
+} from './permissionGuard.composable'
+export { default as PermissionGuard } from './PermissionGuard.vue'
+export type {
+  Register, RegisteredPermission,
+} from './register.type'
