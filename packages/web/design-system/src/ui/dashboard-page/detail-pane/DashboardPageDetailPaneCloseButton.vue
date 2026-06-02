927c2999b8cbe46ad837ec6e596b8fc779684466
JeroenVanC <jeroen.vc1@icloud.com>
2026-06-02 09:38:36 +0200

    fix: remove action from close button

--- packages/web/design-system/src/ui/dashboard-page/detail-pane/DashboardPageDetailPaneCloseButton.vue
+++ packages/web/design-system/src/ui/dashboard-page/detail-pane/DashboardPageDetailPaneCloseButton.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
-import {
-  createAction,
-  useActionGroup,
-} from '@wisemen/vue-core-actions'
-import {
-  LayoutRightIcon,
-  XCloseIcon,
-} from '@wisemen/vue-core-icons'
+import { XCloseIcon } from '@wisemen/vue-core-icons'
 import { useI18n } from 'vue-i18n'
 
-import ActionTrigger from '@/ui/action-trigger/ActionTrigger.vue'
 import { UIIconButton } from '@/ui/button/index'
 import { useInjectMainContentDetailPaneContext } from '@/ui/layout/mainContentDetailPane.context'
 
@@ -18,36 +10,13 @@ const i18n = useI18n()
 const {
   toggle,
 } = useInjectMainContentDetailPaneContext()
-
-const actionGroup = useActionGroup()
-
-const toggleDetailPaneAction = createAction({
-  id: 'toggle-detail-pane',
-  name: () => i18n.t('action.global.toggle_detail_pane.name'),
-  execute: () => {
-    toggle()
-  },
-  group: actionGroup.navigation,
-  icon: () => LayoutRightIcon,
-  keyboardShortcut: {
-    key: 'I',
-    meta: true,
-  },
-  keywords: i18n.t('action.global.toggle_detail_pane.keywords').split(' '),
-})
 </script>
 
 <template>
-  <ActionTrigger
-    :action="toggleDetailPaneAction"
-    :current-context-only="true"
-  >
-    <template #default="{ label }">
-      <UIIconButton
-        :label="label"
-        :icon="XCloseIcon"
-        variant="tertiary"
-      />
-    </template>
-  </ActionTrigger>
+  <UIIconButton
+    :label="i18n.t('component.dashboard_page_detail_pane_toggle.collapse')"
+    :icon="XCloseIcon"
+    variant="tertiary"
+    @click="toggle()"
+  />
 </template>
