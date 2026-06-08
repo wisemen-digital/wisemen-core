f389795b46880d5fd6f6191265d8227da4572b32
Maarten Sijmkens <maarten.sijmkens@wisemen.digital>
2026-06-08 17:32:06 +0200

    utils for monetary classes

--- packages/api/monetary/lib/examples/euros/euros.ts
+++ packages/api/monetary/lib/examples/euros/euros.ts
@@ -1,31 +1,15 @@
 import { createMonetaryUtils } from "../../factory/monetary.factory.js"
-import { Monetary } from "../../monetary.js"
-import { MonetaryDto, MonetaryDtoBuilder } from "../../monetary.dto.js"
-import { MonetaryObject } from "../../monetary.object.js"
 import { Currency } from "../../currency.enum.js"
-
-export class Euros extends Monetary<Currency.EUR> {
-  constructor (object: MonetaryObject<Currency.EUR>);
-  constructor (amount: number, precision: number);
-  constructor (objectOrAmount: MonetaryObject<Currency.EUR> | number, precision?: number) {
-    if (typeof objectOrAmount === 'number') {
-      super(objectOrAmount, Currency.EUR, precision!)
-    } else {
-      super(objectOrAmount)
-    }
-  }
-}
-
-export class EurosDto extends MonetaryDto<Currency.EUR> {}
-
-export class EurosDtoBuilder extends MonetaryDtoBuilder<Currency.EUR> {
-  constructor () {
-    super(Currency.EUR)
-  }
-}
 
 export const {
   Column: EurosColumn,
   ApiProperty: EurosApiProperty,
-  Validator: IsEuros
-} = createMonetaryUtils(Currency.EUR, 2)
+  Validator: IsEuros,
+  Class: Euros,
+  Dto: EurosDto,
+  DtoBuilder: EurosDtoBuilder
+} = createMonetaryUtils(Currency.EUR, 2, 'Euros')
+
+export type Euros = InstanceType<typeof Euros>
+export type EurosDto = InstanceType<typeof EurosDto>
+export type EurosDtoBuilder = InstanceType<typeof EurosDtoBuilder>
--- packages/api/monetary/lib/factory/monetary.factory.ts
+++ packages/api/monetary/lib/factory/monetary.factory.ts
@@ -2,11 +2,17 @@ import { createMonetaryAmountColumn } from "./monetary-amount.column.factory.js"
 import { createMonetaryValidator } from "./monetary.validator.factory.js";
 import { createMonetaryApiProperty } from "./monetary.api-property.factory.js";
 import { Currency } from "../currency.enum.js";
+import { createMonetaryClass } from "./monetary.class.factory.js";
+import { createMonetaryDto } from "./monetary.dto.factory.js";
+import { createMonetaryDtoBuilder } from "./monetary.dto.builder.factory.js";
 
-export function createMonetaryUtils<C extends Currency> (currency: C, monetaryPrecision: number) {
+export function createMonetaryUtils<C extends Currency> (currency: C, monetaryPrecision: number, className: string) {
   return {
     Column: createMonetaryAmountColumn(currency, monetaryPrecision),
     ApiProperty: createMonetaryApiProperty(currency, monetaryPrecision),
-    Validator: createMonetaryValidator(currency, monetaryPrecision)
+    Validator: createMonetaryValidator(currency, monetaryPrecision),
+    Class: createMonetaryClass(currency, className),
+    Dto: createMonetaryDto(currency, className),
+    DtoBuilder: createMonetaryDtoBuilder(currency, className)
   }
 }
