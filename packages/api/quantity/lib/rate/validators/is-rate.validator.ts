363380f0df5b750b6bacc40238750edc5f6c9373
Maarten Sijmkens <maarten.sijmkens@wisemen.digital>
2026-06-08 10:24:45 +0200

    feat(TBN-1122): add rate column utils

---
+++ packages/api/quantity/lib/rate/validators/is-rate.validator.ts
@@ -0,0 +1,16 @@
+import { applyDecorators } from '@nestjs/common'
+import { Type } from 'class-transformer'
+import { IsObject, ValidateNested, ValidationOptions } from 'class-validator'
+import { RateDto } from '../rate.dto.js'
+
+export function IsRate (
+  options?: ValidationOptions
+): PropertyDecorator {
+  const decorators = [
+    Type(() => RateDto),
+    ValidateNested(options),
+    IsObject(options)
+  ]
+
+  return applyDecorators(...decorators)
+}
