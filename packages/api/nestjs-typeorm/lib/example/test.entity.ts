042e151d594977f8d03809a93666868228d0533c
Maarten Sijmkens <maarten.sijmkens@wisemen.digital>
2026-06-08 11:27:20 +0200

    extract example

---
+++ packages/api/nestjs-typeorm/lib/example/test.entity.ts
@@ -0,0 +1,62 @@
+import { Column, Entity } from "typeorm"
+import { Default } from "../types/default.type.js"
+import { Embedded } from "../types/embedded.type.js"
+import { EntityInsert } from "../types/insert.type.js"
+import { Relation } from "../types/relation.js"
+
+@Entity()
+class TestEntity {
+  @Column({ type: 'varchar' })
+  requiredColumn: string
+
+  @Column({ type: 'varchar', nullable: true })
+  optionalColumn: string | null
+
+  @Column({ type: 'varchar', default: 'default value' })
+  defaultColumn: Default<string>
+
+  @Column(() => TestEmbedded)
+  embeddedColumn: Embedded<TestEmbedded>
+
+  relation: Relation<string>
+}
+
+
+class TestEmbedded {
+  @Column({ type: 'varchar' })
+  requiredColumn: string
+
+  @Column({ type: 'varchar', nullable: true })
+  optionalColumn: string | null
+
+  @Column({ type: 'varchar', default: 'default value' })
+  defaultColumn: Default<string>
+}
+
+type TestInsert = EntityInsert<TestEntity>
+
+const _insertOnlyRequired: TestInsert = {
+  requiredColumn: 'required value',
+  embeddedColumn: {
+    requiredColumn: 'required value',
+  },
+  // relation: 'relation value' <-- not allowed
+}
+
+const _insertWithOptionals: TestInsert = {
+  requiredColumn: 'required value',
+  optionalColumn: 'optional value',
+  embeddedColumn: {
+    requiredColumn: 'required value',
+    optionalColumn: 'optional value',
+  }
+}
+
+const _insertOverwriteDefaults: TestInsert = {
+  requiredColumn: 'required value',
+  defaultColumn: 'optional value',
+  embeddedColumn: {
+    requiredColumn: 'required value',
+    defaultColumn: 'optional value',
+  }
+}
--- packages/api/nestjs-typeorm/lib/types/insert.type.ts
+++ packages/api/nestjs-typeorm/lib/types/insert.type.ts
@@ -1,7 +1,6 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm'
-import { Default, HasDefault } from './default.type.js'
-import { Embedded, IsEmbedded } from './embedded.type.js'
-import { IsRelation, Relation } from './relation.js'
+import { HasDefault } from './default.type.js'
+import { IsEmbedded } from './embedded.type.js'
+import { IsRelation } from './relation.js'
 
 type OptionalKeys<T> = {
   [K in keyof T]: null extends T[K]
@@ -32,60 +31,3 @@ export type EntityInsert<T>
           ? EntityInsert<Omit<T[K], '_embedded'>> 
           : T[K] 
     } // optional properties
-
-
-@Entity()
-class TestEntity {
-  @Column({ type: 'varchar' })
-  requiredColumn: string
-
-  @Column({ type: 'varchar', nullable: true })
-  optionalColumn: string | null
-
-  @Column({ type: 'varchar', default: 'default value' })
-  defaultColumn: Default<string>
-
-  @Column(() => TestEmbedded)
-  embeddedColumn: Embedded<TestEmbedded>
-
-  relation: Relation<string>
-}
-
-class TestEmbedded {
-  @Column({ type: 'varchar' })
-  requiredColumn: string
-
-  @Column({ type: 'varchar', nullable: true })
-  optionalColumn: string | null
-
-  @Column({ type: 'varchar', default: 'default value' })
-  defaultColumn: Default<string>
-}
-
-type TestInsert = EntityInsert<TestEntity>
-
-const _insertOnlyRequired: TestInsert = {
-  requiredColumn: 'required value',
-  embeddedColumn: {
-    requiredColumn: 'required value',
-  },
-  // relation: 'relation value' <-- not allowed
-}
-
-const _insertWithOptionals: TestInsert = {
-  requiredColumn: 'required value',
-  optionalColumn: 'optional value',
-  embeddedColumn: {
-    requiredColumn: 'required value',
-    optionalColumn: 'optional value',
-  }
-}
-
-const _insertOverwriteDefaults: TestInsert = {
-  requiredColumn: 'required value',
-  defaultColumn: 'optional value',
-  embeddedColumn: {
-    requiredColumn: 'required value',
-    defaultColumn: 'optional value',
-  }
-}
