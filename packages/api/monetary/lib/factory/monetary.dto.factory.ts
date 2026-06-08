d65b69af7d45e86aeb7e4fb001611d6f9e6a3d44
Maarten Sijmkens <maarten.sijmkens@wisemen.digital>
2026-06-08 17:33:46 +0200

    fix linting

--- packages/api/monetary/lib/factory/monetary.dto.factory.ts
+++ packages/api/monetary/lib/factory/monetary.dto.factory.ts
@@ -1,11 +1,11 @@
 import { Currency } from "../currency.enum.js";
-import { MonetaryDto, MonetaryDtoBuilder } from "../monetary.dto.js";
+import { MonetaryDto } from "../monetary.dto.js";
 
 type MonetaryDtoConstructor<C extends Currency> = {
   new(): MonetaryDto<C>;
 };
 
-export function createMonetaryDto<C extends Currency>(currency: C, className: string): MonetaryDtoConstructor<C> {
+export function createMonetaryDto<C extends Currency>(_currency: C, className: string): MonetaryDtoConstructor<C> {
   const FixedCurrencyMonetaryDto = class extends MonetaryDto<C> { };
 
   Object.defineProperty(FixedCurrencyMonetaryDto, 'name', { value: `${className}Dto` });
