2e036b3e71a47a9b33572c8866bdbec45873040c
Wouter Laermans <wouter.laermans@appwise.be>
2026-06-08 12:02:50 +0200

    fix(api-utils): sort dto type

--- packages/web/api-utils/src/utils/sort/sort.utils.ts
+++ packages/web/api-utils/src/utils/sort/sort.utils.ts
@@ -11,13 +11,14 @@ export class SortUtil {
     sortKeyMap: Record<SortKey, QueryKey>,
   ): {
     key: QueryKey
-    order: SortDirectionDto
+    // Return any instead of SortDirectionDto since enums cannot be compared
+    order: any
   }[] {
     return sort
       .filter((s) => s.direction !== null)
       .map((s) => ({
         key: sortKeyMap[s.key],
-        order: s.direction === 'asc' ? SortDirectionDto.ASC : SortDirectionDto.DESC,
+        order: (s.direction === 'asc' ? SortDirectionDto.ASC : SortDirectionDto.DESC),
       }))
   }
 }
