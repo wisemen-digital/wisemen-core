b80fdab50a14d5d05f6072f621fb17bf3224b100
Wouter Laermans <wouter.laermans@appwise.be>
2026-06-08 15:34:45 +0200

    feat(design-system): table vertical scroll state, border improvements, debounce resize

--- packages/web/design-system/src/ui/table/components/TableBody.vue
+++ packages/web/design-system/src/ui/table/components/TableBody.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 import TableSubgrid from '@/ui/table/components/TableSubgrid.vue'
+import { useInjectTableContext } from '@/ui/table/context/table.context'
+
+const {
+  isScrollableVertically, variant,
+} = useInjectTableContext()
+
+const shouldHideLastRowBorder = computed<boolean>(
+  () => isScrollableVertically.value || variant.value === 'contained',
+)
 </script>
 
 <template>
-  <TableSubgrid class="group/body">
+  <TableSubgrid
+    :class="{
+      '[&>*:last-child]:border-b-0': shouldHideLastRowBorder }
+    "
+    class="group/body"
+  >
     <slot />
   </TableSubgrid>
 </template>
