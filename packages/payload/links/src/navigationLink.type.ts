77203c27a827681b399dd4d605d022f332f9e4ef
Robbe <robbevaes95@gmail.com>
2026-07-09 09:26:44 +0200

    feat: add customisable types

--- packages/payload/links/src/navigationLink.type.ts
+++ packages/payload/links/src/navigationLink.type.ts
@@ -1,35 +1,51 @@
 import type { LinkFieldDocument } from '#link.type.ts'
+import type {
+  LinkableCollectionSlug,
+  NavigationLinkEventValue,
+} from '#links.registry.ts'
 
-export interface NavigationDropdownItemDocument {
-  event?: string | null
+export interface NavigationDropdownItemDocument<
+  TRelationTo extends string = LinkableCollectionSlug,
+  TEvent extends string = NavigationLinkEventValue,
+> {
+  event?: TEvent | null
   label: string
-  link?: LinkFieldDocument | null
+  link?: LinkFieldDocument<TRelationTo> | null
   navType: 'event' | 'link'
 }
 
-export interface NavigationDropdownDocument {
+export interface NavigationDropdownDocument<
+  TRelationTo extends string = LinkableCollectionSlug,
+  TEvent extends string = NavigationLinkEventValue,
+> {
   label: string
-  links?: NavigationDropdownItemDocument[] | null
+  links?: NavigationDropdownItemDocument<TRelationTo, TEvent>[] | null
   navType: 'dropdown'
 }
 
-export interface NavigationEventDocument {
-  event?: string | null
+export interface NavigationEventDocument<TEvent extends string = NavigationLinkEventValue> {
+  event?: TEvent | null
   label: string
   navType: 'event'
 }
 
-export interface NavigationLinkRelationDocument {
+export interface NavigationLinkRelationDocument<TRelationTo extends string = LinkableCollectionSlug> {
   label: string
-  link?: LinkFieldDocument | null
+  link?: LinkFieldDocument<TRelationTo> | null
   navType: 'link'
 }
 
-export type NavigationLinkDocument
-  = | NavigationDropdownDocument
-    | NavigationEventDocument
-    | NavigationLinkRelationDocument
+export type NavigationLinkDocument<
+  TRelationTo extends string = LinkableCollectionSlug,
+  TEvent extends string = NavigationLinkEventValue,
+>
+  = | NavigationDropdownDocument<TRelationTo, TEvent>
+    | NavigationEventDocument<TEvent>
+    | NavigationLinkRelationDocument<TRelationTo>
 
-export interface HeaderNavigationRowDocument {
-  navLink: NavigationLinkDocument
+export interface HeaderNavigationRowDocument<
+  TRelationTo extends string = LinkableCollectionSlug,
+  TEvent extends string = NavigationLinkEventValue,
+> {
+  navLink: NavigationLinkDocument<TRelationTo, TEvent>
 }
