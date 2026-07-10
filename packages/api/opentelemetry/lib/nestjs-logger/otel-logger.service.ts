9ae2e561bf7777f3382cf25caad3b4c59db87f45
Pieter Pauwels <pieter.pauwels.pp@gmail.com>
2026-07-10 12:25:12 +0200

    feat(tbn-403): NestJs OpenTelemetry logger service

---
+++ packages/api/opentelemetry/lib/nestjs-logger/otel-logger.service.ts
@@ -0,0 +1,77 @@
+import { Inject, Injectable, type LoggerService, type LogLevel, type OnModuleInit } from '@nestjs/common'
+import { LogRecord, OpenTelemetryLogger } from '../logging.js'
+import { MODULE_OPTIONS_TOKEN } from './otel-logger.module-definitions.js'
+import { OtelLoggerModuleOptions } from './otel-logger.module-options.js'
+
+const DEFAULT_LOG_LEVELS: LogLevel[] = [
+  'log',
+  'error',
+  'warn',
+  'debug',
+  'fatal'
+]
+
+@Injectable()
+export class NestjsOtelLogger implements LoggerService, OnModuleInit {
+  private _logger: OpenTelemetryLogger | undefined
+
+  constructor (
+    @Inject(MODULE_OPTIONS_TOKEN) private config: OtelLoggerModuleOptions
+  ) {}
+
+  onModuleInit () {
+    this._logger = new OpenTelemetryLogger(this.config)
+  }
+
+  private get logger (): OpenTelemetryLogger {
+    if (this._logger === undefined) {
+      throw new Error('Logger is not initialized')
+    }
+
+    return this._logger
+  }
+
+  private get logLevels (): LogLevel[] {
+    return this.config.logLevels ?? DEFAULT_LOG_LEVELS
+  }
+
+  log (logRecord: LogRecord) {
+    if (!this.logLevels.includes('log')) {
+      return
+    }
+
+    this.logger.info(logRecord)
+  }
+
+  error (logRecord: LogRecord) {
+    if (!this.logLevels.includes('error')) {
+      return
+    }
+
+    this.logger.error(logRecord)
+  }
+
+  warn (logRecord: LogRecord) {
+    if (!this.logLevels.includes('warn')) {
+      return
+    }
+
+    this.logger.warn(logRecord)
+  }
+
+  debug (logRecord: LogRecord) {
+    if (!this.logLevels.includes('debug')) {
+      return
+    }
+    
+    this.logger.debug(logRecord)
+  }
+
+  fatal (logRecord: LogRecord) {
+    if (!this.logLevels.includes('fatal')) {
+      return
+    }
+
+    this.logger.fatal(logRecord)
+  }
+}
