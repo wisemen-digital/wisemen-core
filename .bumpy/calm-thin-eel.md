3ca1dfa193dc6163297efe135be207889d4e9549
Kobe Kwanten <kobe.kwanten@wisemen.digital>
2026-07-24 12:57:37 +0200

    feat: remove serializable constraint from base job data

---
+++ .bumpy/calm-thin-eel.md
@@ -0,0 +1,5 @@
+---
+"@wisemen/pgboss-nestjs-job": minor
+---
+
+feat: remove serializable constraint from base job data
--- packages/api/pgboss-nestjs-job/lib/jobs/base-job.ts
+++ packages/api/pgboss-nestjs-job/lib/jobs/base-job.ts
@@ -1,16 +1,8 @@
 import { JobInsert } from 'pg-boss'
 
-type Primitive = string | number | boolean | null | undefined
-type Serializable = {
-  [key: string | number | symbol]: Serializable | Serializable[] | Primitive | Primitive[]
-}
-
-export type BaseJobData = Serializable
-
 type JobOptions = Omit<JobInsert, 'id' | 'name' | 'data'>
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export abstract class BaseJob<T extends BaseJobData = {}> {
+ 
+export abstract class BaseJob<T = object> {
   constructor (
     readonly data: T = {} as T,
     readonly options?: JobOptions
