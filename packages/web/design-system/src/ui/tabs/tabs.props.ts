62545da380a989fb74865cab0527fa0f0a629e1c
JeroenVanC <jeroen.vc1@icloud.com>
2026-07-27 14:07:53 +0200

    feat: add dot indicitor to tabs + change api for tabs config

--- packages/web/design-system/src/ui/tabs/tabs.props.ts
+++ packages/web/design-system/src/ui/tabs/tabs.props.ts
@@ -2,6 +2,7 @@ import type { Component } from 'vue'
 
 import type { RegisteredRouteLocationRaw } from '@/register'
 import type { DisabledWithReason } from '@/types/disabledWithReason.type'
+import type { TabsItemConfig } from '@/ui/tabs/tabs.type'
 
 export type TabsVariant = 'button-border' | 'button-brand' | 'underline'
 export type TabsHorizontalListPadding = 'lg' | 'md' | 'none' | 'sm' | 'xl'
@@ -35,7 +36,13 @@ export interface TabsItemProps extends DisabledWithReason {
    */
   isLabelHidden?: boolean
   /**
+   * Configuration object for the tab's left content and indicator.
+   */
+  config?: TabsItemConfig | null
+  /**
    * A count to display as a badge on the right side of the tab.
+   *
+   * @deprecated Use `config.indicator` instead.
    */
   count?: number | null
   /**
@@ -46,6 +53,8 @@ export interface TabsItemProps extends DisabledWithReason {
   disabledReason?: string | null
   /**
    * An optional icon to be displayed alongside the tab label.
+   *
+   * @deprecated Use `config.left` instead.
    */
   icon?: Component
   /**
@@ -78,7 +87,13 @@ export interface TabsRouterLinkItemProps extends DisabledWithReason {
    */
   isLabelHidden?: boolean
   /**
+   * Configuration object for the tab's left content and indicator.
+   */
+  config?: TabsItemConfig | null
+  /**
    * A count to display as a badge on the right side of the tab.
+   *
+   * @deprecated Use `config.indicator` instead.
    */
   count?: number | null
   /**
@@ -89,6 +104,8 @@ export interface TabsRouterLinkItemProps extends DisabledWithReason {
   disabledReason?: string | null
   /**
    * An optional icon to be displayed alongside the tab label.
+   *
+   * @deprecated Use `config.left` instead.
    */
   icon?: Component
   /**
